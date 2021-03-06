81a3ff58bb8ceab6e7856bb4ac0d1b3f4bad3ba0
edmbernard <bernard.erwan@gmail.com>
2021-03-06 16:08:26 +0100

    feat: add action implementation

--- src/controller.h
+++ src/controller.h
@@ -5,6 +5,8 @@
 #include <string>
 #include <tuple>
 #include <vector>
+#include <iostream>
+#include <assert.h>
 
 #include <spdlog/spdlog.h>
 
@@ -14,45 +16,69 @@
 
 class Controller {
 public:
-  Controller(int numberRuns) : m_numberRuns(numberRuns) {
-    spdlog::info("Number of bots registered : {}", GetBotRegister().size());
-    botIndex = std::vector<int>(GetBotRegister().size(), 0);
+  Controller(int numberRuns)
+    : m_numberRuns(numberRuns)
+    , bots(GetBotRegister())
+    , botIndex(bots.size(), 0)
+    , cities(bots.size())
+  {
     std::iota(botIndex.begin(), botIndex.end(), 0);
+
+    assert(botIndex.size() == bots.size());
+    assert(cities.size() == bots.size());
+
+    spdlog::info("Number of bots registered : {}", bots.size());
   }
 
   void run() {
 
     std::random_device rd;
     std::mt19937 gen(rd());
-    const auto botList = GetBotRegister();
-
     spdlog::info("Run Start");
     for (int step = 1; step <= m_numberRuns; ++step) {
       spdlog::debug("Iteration : {:05}", step);
 
+      // Phase 1: Mutation
+
+      // Phase 2: Reproduction
+
+      // Phase 3: Migration
+
+      // Phase 4: Infection
+
+      // Phase 5: Contagion
+
+      // Phase 6: Extinction
+
+      // Phase 7: Players turn
       std::shuffle(botIndex.begin(), botIndex.end(), gen);
       for (int id : botIndex) {
-        const auto &[name, bot] = botList[id];
-        BotReturn actions = bot(step, id, 1);
+        const auto &[name, bot] = bots[id];
+        for (auto action : bot(step, id, 1)) {
+          doAction(action, cities[id]);
+        }
       }
+
     }
     spdlog::info("Run End");
   }
 
-  static std::vector<std::tuple<std::string, std::function<std::array<Action, 3>(int, int, int)>>> &GetBotRegister() {
-    static std::vector<std::tuple<std::string, std::function<std::array<Action, 3>(int, int, int)>>> g_bots;
+  static std::vector<std::tuple<std::string, BotFunction>> &GetBotRegister() {
+    static std::vector<std::tuple<std::string, BotFunction>> g_bots;
     return g_bots;
   }
 
 private:
+  std::vector<std::tuple<std::string, BotFunction>> bots;
   std::vector<int> botIndex;  // Bot index to shuffle execution order for each step
+  std::vector<City> cities;
   int m_numberRuns;
 };
 
 
 class RegisterBot {
 public:
-  RegisterBot(const std::string &name, std::function<std::array<Action, 3>(int, int, int)> bot) {
+  RegisterBot(const std::string &name, BotFunction bot) {
     Controller::GetBotRegister().push_back({name, bot});
   }
 };
--- src/engine.h
+++ src/engine.h
@@ -5,22 +5,75 @@
 #pragma once
 
 enum class Action {
-  kDoNoThing,               // Do Nothing
+  kDoNoThing,             // Do Nothing
   kResearchEpidemiology,  // Research Epidemiology [Effects: Reduce local Contagion Rate by 8%]
   kResearchImmunology,    // Research Immunology [Effects: Reduce local Lethality Rate by 4%]
   kResearchVaccination,   // Research Vaccination [Effects: Reduce local Contagion Rate by 4%, reduce local Lethality Rate by 2%]
   kGiveCure,    // Give Cure [Effects: Convert 10 local Infected to Healthy]
-  kQuarantine,  // Quarantine [Effects: Isolate 30 local Infected for one Turn, they will be remove from the Contagion computation]
+  kQuarantine,  // Quarantine [Effects: Isolate 30 local people for one Turn, they will be remove from the Contagion computation]
   kKill,        // Kill [Effects: Remove 30 local Infected]
 };
+
+constexpr int kValueResearchEpidemiology = 8;
+constexpr int kValueResearchImmunology = 4;
+constexpr int kValueResearchVaccination = 2;
+constexpr int kValueGiveCure = 10;
+constexpr int kValueQuarantine = 30;
+constexpr int kValueKill = 30;
 
 using BotReturn = std::array<Action, 3>;
 using BotFunction = std::function<BotReturn(int, int, int)>;
 
 struct City {
-  int healty = 99;
+  int healthy = 99;
   int infected = 1;
-  int isolated = 0;
+  int healthyIsolated = 0;
+  int infectedIsolated = 0;
   int contagionRate = 5;
   int lethalityRate = 5;
 };
+
+inline void decrease(int &variable, int value) {
+  if (variable > value) {
+    variable -= value;
+  } else {
+    variable = 0;
+  }
+}
+
+inline void doAction(Action action, City &city) {
+
+  switch (action) {
+    case Action::kResearchEpidemiology:
+      decrease(city.contagionRate, kValueResearchEpidemiology);
+      break;
+
+    case Action::kResearchImmunology:
+      decrease(city.lethalityRate, kValueResearchImmunology);
+      break;
+
+    case Action::kResearchVaccination:
+      decrease(city.contagionRate, kValueResearchVaccination * 2);
+      decrease(city.lethalityRate, kValueResearchVaccination);
+      break;
+
+    case Action::kGiveCure: {
+        const int cured = city.infected >= kValueGiveCure ? kValueGiveCure : city.infected;
+        city.infected -= cured;
+        city.healthy += cured;
+      }
+      break;
+    case Action::kQuarantine: {
+        const int healthyIsolated = kValueQuarantine * city.healthy / (city.infected + city.healthy);
+        decrease(city.healthy, healthyIsolated);
+        city.healthyIsolated = healthyIsolated;
+        const int infectedIsolated = kValueQuarantine - healthyIsolated;
+        decrease(city.infected, infectedIsolated);
+        city.infectedIsolated = infectedIsolated;
+      }
+      break;
+    case Action::kKill:
+      decrease(city.infected, kValueKill);
+      break;
+  }
+}
