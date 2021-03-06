0993ac1c8f316cc72f9827d75b6a3e25b30bbb5e
edmbernard <bernard.erwan@gmail.com>
2021-03-06 16:59:21 +0100

    feat: add phase

--- src/controller.h
+++ src/controller.h
@@ -36,23 +36,66 @@ public:
 
     std::random_device rd;
     std::mt19937 gen(rd());
+
+    std::uniform_int_distribution<int> mutationAction(0, 1);
+
     spdlog::info("Run Start");
     for (int step = 1; step <= m_numberRuns; ++step) {
       spdlog::debug("Iteration : {:05}", step);
 
       // Phase 1: Mutation
+      for (auto &city : cities) {
+        switch (mutationAction(gen)) {
+          case 0:
+            city.contagionRate += 5;
+            break;
+          case 1:
+            city.lethalityRate += 5;
+            break;
+          default:
+            throw std::runtime_error("Mutation action out of range");
+        }
+      }
 
       // Phase 2: Reproduction
+      if (step % 5 == 0) {
+        for (auto &city : cities) {
+          city.healthy += city.healthy / 2;
+          city.infected += city.infected / 2;
+        }
+      }
 
       // Phase 3: Migration
 
-      // Phase 4: Infection
-
       // Phase 5: Contagion
+      for (auto &city : cities) {
+        if (city.contagionRate == 0) {
+          continue;
+        }
+        const int toInfect = (city.infected + city.contagionRate -1) / city.contagionRate;
+        const int converted = city.healthy >= toInfect ? toInfect : city.healthy;
+        city.infected += converted;
+        city.healthy -= converted;
+      }
 
       // Phase 6: Extinction
+      for (auto &city : cities) {
+        if (city.lethalityRate == 0) {
+          continue;
+        }
+        const int dead = (city.infected + city.lethalityRate -1) / city.lethalityRate;
+        decrease(city.infected, dead);
+      }
 
-      // Phase 7: Players turn
+      // Phase 7: Free from quarantine
+      for (auto &city : cities) {
+        city.healthy += city.healthyIsolated;
+        city.infected += city.infectedIsolated;
+        city.healthyIsolated = 0;
+        city.infectedIsolated = 0;
+      }
+
+      // Phase: Players turn
       std::shuffle(botIndex.begin(), botIndex.end(), gen);
       for (int id : botIndex) {
         const auto &[name, bot] = bots[id];
@@ -75,9 +118,9 @@ public:
       return false;
     });
 
-    fmt::print("| {:>20} | {:>10} | {:>10} |\n", "Bot Name", "Healthy", "Infected");
+    fmt::print("| {:>20} | {:>10} | {:>10} | {:>10} | {:>10} |\n", "Bot Name", "Healthy", "Infected", "Contagion", "Lethality");
     for (int id : botIndex) {
-      fmt::print("| {:>20} | {:>10} | {:>10} |\n", std::get<0>(bots[id]), cities[id].healthy, cities[id].infected);
+      fmt::print("| {:>20} | {:>10} | {:>10} | {:>9}% | {:>9}% |\n", std::get<0>(bots[id]), cities[id].healthy, cities[id].infected, cities[id].contagionRate, cities[id].lethalityRate);
     }
   }
 
